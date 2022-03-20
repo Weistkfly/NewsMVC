78c0f0dda534302dd23030754a19d735999d458a
Jesus (Weistkfly) Cedeno <jesuscvcedenoc@gmail.com>
2022-03-20 17:35:57 -0400

    Added Categories

--- NewsMVC2019-2820/Controllers/CategoryController.cs
+++ NewsMVC2019-2820/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewsMVC2019_2820.Data;
+using NewsMVC2019_2820.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,62 @@ namespace NewsMVC2019_2820.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
